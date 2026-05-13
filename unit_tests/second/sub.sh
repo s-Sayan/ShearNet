79c5904c580b17079024155c3b09f1a3eddd77c9
Adam Field <adfield@login-02.cm.cluster>
2026-05-13 01:44:42 -0400

    timing script

--- unit_tests/second/sub.sh
+++ unit_tests/second/sub.sh
@@ -11,3 +11,7 @@ echo "Submitted m job: $JOBID_M (depends on $JOBID_TRAIN)"
 # Submit leakage job, after training completes
 JOBID_PSF=$(sbatch --dependency=afterok:$JOBID_TRAIN ./benchmarking/psf_leakage/run_leakage.sh | awk '{print $4}')
 echo "Submitted leakage job: $JOBID_PSF (depends on $JOBID_TRAIN)"
+
+# Submit timing job, after training completes
+JOBID_TIMING=$(sbatch --dependency=afterok:$JOBID_TRAIN ./timing/run_timing.sh | awk '{print $4}')
+echo "Submitted leakage job: $JOBID_TIMING (depends on $JOBID_TRAIN)"
